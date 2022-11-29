4548ce1a342961cccb350f77f6169e1e7b272587
JeongSoong <118896711+JeongSoong@users.noreply.github.com>
2022-11-29 20:00:20 +0900

    Add files via upload

---
+++ 20221129/20221129.ino
@@ -0,0 +1,50 @@
+#define IN1 2
+#define IN2 3
+#define ENA 5
+
+#define IN3 4
+#define IN4 7
+#define ENB 6
+
+void setup()
+{
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+}
+
+void loop()
+{
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, 130);
+  analogWrite(ENB, 130);
+  delay(2000);
+
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 130);
+  analogWrite(ENB, 130);
+  delay(1300);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+  analogWrite(ENA, 130);
+  analogWrite(ENB, 130);
+  delay(1400);
+
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(ENA, 130);
+  analogWrite(ENB, 130);
+  delay(2000);
+}
